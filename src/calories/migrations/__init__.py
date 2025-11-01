c3635296a7a22a705e38a89a3f1b25c5389a464a
Mobin Yousefi <mobinyousefi.cs@gmail.com>
2025-11-01 19:31:03 +0330

    Add files via upload

---
+++ src/calories/migrations/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+===========================================================================
+Project: Django Calorie Calculator
+File: src/calories/migrations/__init__.py
+Author: Mobin Yousefi (GitHub: github.com/mobinyousefi-cs)
+Created: 2025-11-01
+Updated: 2025-11-01
+License: MIT License (see LICENSE file for details)
+===========================================================================
+
+Initialize Django project package.
+===========================================================================
+"""
+
+# Empty
---
+++ src/config/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+===========================================================================
+Project: Django Calorie Calculator
+File: src/config/__init__.py
+Author: Mobin Yousefi (GitHub: github.com/mobinyousefi-cs)
+Created: 2025-11-01
+Updated: 2025-11-01
+License: MIT License (see LICENSE file for details)
+===========================================================================
+
+Initialize Django project package.
+===========================================================================
+"""
+
+# This file intentionally left blank.
+# Marks the directory as a Python package.
+# All Django settings and routing are defined in sibling modules:
+# - settings.py: global configurations
+# - urls.py: URL patterns
+# - wsgi.py / asgi.py: server entry points
